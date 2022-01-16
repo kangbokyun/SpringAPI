877888b76c855841e986e711dd74336107189d4f
강보균 <bk940204@naver.com>
2022-01-16 22:23:44 +0900

    DB에 하나씩 넣기, 넣고 중복곡은 들어가지 않게 하기

--- src/main/java/Melon/Controller/NewSongController.java
+++ src/main/java/Melon/Controller/NewSongController.java
@@ -1,12 +1,23 @@
 package Melon.Controller;
 
+import Melon.Service.NewSongService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NewSongController {
+	@Autowired
+	NewSongService newSongService;
+
 	@GetMapping("/")
 	public String goToMain() {
 		return "Melon/Main";
+	}
+
+	@GetMapping("NewSong/NewSongList")
+	public String goToNewSong() {
+		newSongService.NewSongSave();
+		return "Melon/NewSong/NewSongList";
 	}
 }
---
+++ src/main/java/Melon/Service/NewSongService.java
@@ -0,0 +1,72 @@
+package Melon.Service;
+
+import Melon.Domain.DTO.NewSongDTO;
+import Melon.Domain.Entity.NewSongRepository;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class NewSongService {
+	@Autowired
+	NewSongRepository newSongRepository;
+	public String NewSongSave() {
+		try {
+			JsonObject melonDataOBJ = new JsonObject();
+			JsonArray songInfo = new JsonArray();
+
+			// 인터넷 코드와 연결
+			Document document1 = Jsoup.connect("https://www.melon.com/new/index.htm").get();
+			// 클래스 명으로 큰 틀 지정
+//                        Elements melon_song = document1.getElementsByClass("wrap_song_info");
+//			// 큰 틀에서 태그를 통해 타고 원하는 정보까지 들어감
+//                        Elements file = melon_song.select("div>div>span>a");
+
+			// 노래 제목 뽑기
+			Elements melon_song = document1.getElementsByClass("ellipsis rank01"); // 클래스 명으로 큰 틀 지정
+			Elements songTitle = melon_song.select("span>a"); // 큰 틀에서 태그를 통해 타고 원하는 정보까지 들어감
+			// 가수명 뽑기
+			Elements melon_singer = document1.getElementsByClass("ellipsis rank02"); // 클래스 명으로 큰 틀 지정
+			Elements singerName = melon_singer.select("span"); // 큰 틀에서 태그를 통해 타고 원하는 정보까지 들어감
+			// 앨범컷 뽑기
+			Elements song_img = document1.getElementsByClass("image_typeAll"); // 클래스 명으로 큰 틀 지정
+			Elements imgName = song_img.select("img"); // 큰 틀에서 태그를 통해 타고 원하는 정보까지 들어감
+			// 좋아요 뽑기
+			Elements songLike = document1.getElementsByClass("cnt");
+
+//			System.out.println(songLike.text());
+
+			ArrayList<String> songAndSinger = new ArrayList<>(); // 곡명과 가수명을 합칠 리스트
+			String[] songArray = new String[melon_song.size()]; // 곡명
+			String[] singerArray = new String[melon_song.size()]; // 가수명
+			String[] songIMGArray = new String[melon_song.size()];
+
+			NewSongDTO newSongDTO = new NewSongDTO();
+
+			for(int i = 0; i < melon_song.size(); i++) {
+				songArray[i] = songTitle.get(i).text().split("\n")[0];
+				singerArray[i] = singerName.get(i).text().split("\n")[0];
+				// attr("abs:src")는 src라는 속성 값의 절대 경로를 달라는 뜻이므로 URL 뿐 아니라 도메인도 붙어서 오게 된다.
+				songIMGArray[i] = imgName.get(i).attr("abs:src");
+				newSongDTO.setS_title(songArray[i]);
+				newSongDTO.setS_singer(singerArray[i]);
+				newSongDTO.setS_img(songIMGArray[i]);
+			}
+			newSongRepository.save(newSongDTO.newSongEntity());
+
+			System.out.println(newSongDTO.getS_title() + ", " + newSongDTO.getS_singer() + ", " + newSongDTO.getS_img());
+
+//			System.out.println(singerName);
+			return "";
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			return "";
+		}
+	}
+}
