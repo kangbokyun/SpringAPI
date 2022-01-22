b0436ffa6459cdb370beb07b060dabc94653cff4
강보균 <bk940204@naver.com>
2022-01-22 15:41:55 +0900

    신곡 중복 끝ㄹㅇ

--- src/main/resources/static/JS/NewSongJS.js
+++ src/main/resources/static/JS/NewSongJS.js
@@ -3,10 +3,7 @@ function newSongUpdate() {
                 url: "/NewSong/NewSongUpdate",
                 success: function(result) {
                         if(result == 1) {
-                                alert("최신곡이 업데이트 됐습니다.");
                                 location.href = "/NewSong/NewSongList";
-                        } else {
-                                console.log("Failed");
                         }
                 }
         });
