ddf143c08aea92293c4f5a0bbf93b6b682e62109
kangbokyun <91529033+kangbokyun@users.noreply.github.com>
2022-09-06 13:10:09 +0900

    Update NewSongService.java

--- src/main/java/Melon/Service/NewSongService.java
+++ src/main/java/Melon/Service/NewSongService.java
@@ -14,7 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service; 
 
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
