0cc1201a166b1367ecd788c588940dadd7ea7027
강보균 <bk940204@naver.com>
2022-09-20 14:03:59 +0900

    푸터 레이아웃 변경

--- src/main/resources/static/JS/Main.js
+++ src/main/resources/static/JS/Main.js
@@ -45,9 +45,11 @@ var Graph = setInterval(function test() {
 
 function OverFooter(no) {
     if(no == 1) {
-        $("#divOver").html("<div style = 'border : solid 1px black; text-align : center;' id = 'divOver'><h4>개인정보 처리방침</h4><p>회원가입 시 등록한 개인정보는 어떠한 곳에도 이용하지 않습니다.<p>될 수 있으면 매일 저녁 22시 언저리에 회원 DB내용 삭제합니다.</div>");
+        $("#divOver").html("<div style = 'border : solid 1px black; text-align : center;' id = 'divOver'><h4>개인정보 처리방침</h4><p style = 'margin-bottom : 5px;'>회원가입 시 등록한 개인정보는 어떠한 곳에도 이용하지 않습니다.<p style = 'margin-bottom : 5px; margin-top : 5px;'>될 수 있으면 매일 저녁 22시 언저리에 회원 DB내용 삭제합니다.</div>");
+    } else if(no == 2) {
+        $("#divOver").html("<div style = 'border : solid 1px black; text-align : center;' id = 'divOver'><h4>운영정책</h4><p style = 'margin-bottom : 5px;'>로컬 서버를 이용하거나 AWS를 이용해 배포합니다.<p>크롤링 및 스크래핑된 정보를 통한 어떠한 수익도 발생하지 않습니다.</div>");
     } else {
-        $("#divOver").html("<div style = 'border : solid 1px black; text-align : center;' id = 'divOver'><h4>운영정책</h4><p>로컬 서버를 이용하거나 AWS를 이용해 배포합니다.<p>크롤링 및 스크래핑된 정보를 통한 어떠한 수익도 발생하지 않습니다.<p>문제가 되는 부분이 있다면 아래 메일로 연락 바랍니다.<p>bk940204@naver.com</div>");
+        $("#divOver").html("<div style = 'border : solid 1px black; text-align : center;' id = 'divOver'><h4>문의 및 건의</h4><p style = 'margin-bottom : 5px;'>문제가 되는 부분이 있거나 문의하실 내용이 <p style = 'margin-bottom : 5px; margin-top : 5px;'>있다면 아래 메일로 연락 바랍니다.<p style = 'margin-bottom : 5px; margin-top : 5px;'>bk940204@naver.com</div>");
     }
 }
 function LeaveFooter() {
