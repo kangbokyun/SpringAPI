624f079964bd0024fc54534d551941bda0fcff86
kangbokyun <91529033+kangbokyun@users.noreply.github.com>
2022-09-22 15:11:22 +0900

    Update Melon.java

--- JSON/Crawling/Melon.java
+++ JSON/Crawling/Melon.java
@@ -6,7 +6,7 @@ import com.google.gson.JsonObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.jsoup.select.Elements; 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
